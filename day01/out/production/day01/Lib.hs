f825044ffa5b333e4944123b452ca9c0386c7b3c
willGuimont <wil9876543210@hotmail.com>
2019-12-01 13:15:29 -0500

    day 1

---
+++ day01/out/production/day01/Lib.hs
@@ -0,0 +1,30 @@
+module Lib
+  ( solve
+  , solve2
+  ) where
+
+apply :: a -> (a -> b) -> b
+apply x f = f x
+
+infixl 0 |>
+
+(|>) :: a -> (a -> b) -> b
+x |> f = apply x f
+
+solve :: IO ()
+solve = do
+  content <- readFile "input.txt"
+  let linesOfFiles = lines content
+  linesOfFiles |> map read |> map (\x -> x `quot` 3 - 2) |> sum |> print
+
+computeFuelNeeded :: Integer -> Integer
+computeFuelNeeded x
+  | v > 0 = v + computeFuelNeeded v
+  | otherwise = 0
+  where v = x `quot` 3 - 2
+
+solve2 :: IO ()
+solve2 = do
+  content <- readFile "input.txt"
+  let linesOfFiles = lines content
+  linesOfFiles |> map read |> map computeFuelNeeded |> sum |> print
