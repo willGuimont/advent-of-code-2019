2212cdb8eded07e85e601c067a14275594c5d841
willGuimont <wil9876543210@hotmail.com>
2019-12-11 16:17:25 -0500

    checkpoint

--- day10/out/production/day10/Lib.hs
+++ day10/out/production/day10/Lib.hs
@@ -2,8 +2,9 @@ module Lib
   ( solve
   ) where
 
-import Control.Monad (guard, forM, forM_)
 import qualified Data.Set as S
+import Data.List (maximumBy)
+import Control.Monad (guard)
 
 positions :: [String] -> S.Set (Integer, Integer)
 positions xs =
@@ -16,7 +17,7 @@ positions xs =
 roundTo n f = fromInteger (round $ f * (10 ^ n)) / (10.0 ^^ n)
 
 angle :: (Integer, Integer) -> (Integer, Integer) -> Double
-angle (x1, y1) (x2, y2) = atan2 dy dx
+angle (x1, y1) (x2, y2) = atan2 (-dx) dy
   where
     dx = fromInteger $ x2 - x1
     dy = fromInteger $ y2 - y1
@@ -26,6 +27,9 @@ solve = do
   content <- readFile "input.txt"
   let linesOfFiles = lines content
   let pos = positions linesOfFiles
-  let all = S.map (\x -> S.size $ S.map (roundTo 5 . angle x) pos) pos 
+  let all = S.map (\x -> (S.size $ S.map (roundTo 5 . angle x) pos, x)) pos
+  let (count, position) = maximumBy (\x y -> compare (fst x) (fst y)) all
+  print position -- (19,11)
   
-  print $ maximum all
+  
+  
