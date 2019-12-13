8733f2a7d079b6d45e7849cf9db2d1751f7857ae
willGuimont <wil9876543210@hotmail.com>
2019-12-12 22:22:52 -0500

    day 12

--- day12/day12.py
+++ day12/day12.py
@@ -1,10 +1,11 @@
 import numpy as np
+import itertools
 
 
 class Moon:
-    def __init__(self, x, y, z):
-        self.position = np.array([x, y, z])
-        self.velocity = np.array([0, 0, 0])
+    def __init__(self, p, v):
+        self.position = np.array(p)
+        self.velocity = np.array(v)
 
     def update(self):
         self.position += self.velocity
@@ -26,11 +27,11 @@ class Moon:
         return self.kin() * self.pot()
 
 
-if __name__ == '__main__':
-    a = Moon(-10, -13, 7)
-    b = Moon(1, 2, 1)
-    c = Moon(-15, -3, 13)
-    d = Moon(3, 7, -4)
+def solve1():
+    a = Moon([-10, -13, 7], [0, 0, 0])
+    b = Moon([1, 2, 1], [0, 0, 0])
+    c = Moon([-15, -3, 13], [0, 0, 0])
+    d = Moon([3, 7, -4], [0, 0, 0])
 
     moons = [a, b, c, d]
 
@@ -43,3 +44,39 @@ if __name__ == '__main__':
 
     tot = sum(map(lambda x: x.tot(), moons))
     print(tot)
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a * b / gcd(a, b)
+
+
+def find_cycle(start):
+    n = len(start)
+    moons = list(map(lambda x: Moon([x], [0]), start))
+
+    for i in itertools.count(1, 1):
+        for m1 in moons:
+            for m2 in moons:
+                m1.attract(m2)
+        for m in moons:
+            m.update()
+        if list(map(lambda x: x.position, moons)) == start and list(map(lambda x: x.velocity, moons)) == [0] * n:
+            return i
+
+
+def solve2():
+    a = find_cycle([-10, 1, -15, 3])
+    b = find_cycle([-13, 2, -3, 7])
+    c = find_cycle([7, 1, 13, -4])
+
+    print(lcm(a, lcm(b, c)))
+
+
+if __name__ == '__main__':
+    solve2()
